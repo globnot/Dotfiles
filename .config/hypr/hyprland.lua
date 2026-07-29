374d2ae18d995e4eb879d62890f9df6732a40e22
Globnot <antoineborda-pro@proton.me>
2026-07-29 16:42:30 +0200

    Tune window gaps (in=4, out=8)

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -120,8 +120,8 @@ hl.env("HYPRCURSOR_SIZE", "24")
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 5,
-        gaps_out = 20,
+        gaps_in  = 4,
+        gaps_out = 8,
 
         border_size = 2,
 
